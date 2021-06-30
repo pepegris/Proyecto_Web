18a13cdac88926636ff5a28f1a33283a72faa160
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2021-06-30 10:39:05 -0400

    up

--- assets/includes/header.php
+++ assets/includes/header.php
@@ -11,6 +11,9 @@
 
     <link href="assets/images/ico/ico.png" rel="icon">
   <link href="assets/images/ico/ico.png" rel="apple-touch-icon">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+
 
 
   
